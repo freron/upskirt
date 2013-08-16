3075106895d59aade7471dfde85c0d77fe83614c
Benny Kjær Nielsen <github@freron.com>
2013-08-16 14:13:49 +0200

    Changed: Trailing whitespace is not allowed for header lines since it does not work well in emails. In particular for signature separators.

--- src/markdown.c
+++ src/markdown.c
@@ -1395,13 +1395,13 @@ is_headerline(uint8_t *data, size_t size)
 	/* test of level 1 header */
 	if (data[i] == '=') {
 		for (i = 1; i < size && data[i] == '='; i++);
-		while (i < size && data[i] == ' ') i++;
+		// while (i < size && data[i] == ' ') i++; // Trailing whitespace is bad in emails.
 		return (i >= size || data[i] == '\n') ? 1 : 0; }
 
 	/* test of level 2 header */
 	if (data[i] == '-') {
 		for (i = 1; i < size && data[i] == '-'; i++);
-		while (i < size && data[i] == ' ') i++;
+		// while (i < size && data[i] == ' ') i++; // Trailing whitespace is bad in emails.
 		return (i >= size || data[i] == '\n') ? 2 : 0; }
 
 	return 0;
